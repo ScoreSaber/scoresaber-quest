7e73978d9b1d75a08f5a06708bf228c8b2200966
Umbranox <itsumbra@protonmail.com>
2022-04-26 08:12:43 +1000

    Fix mod data path

--- include/static.hpp
+++ include/static.hpp
@@ -1,11 +1,10 @@
 #include <string>
 namespace ScoreSaber::Static
 {
-    // http://192.168.1.8:9999
     // https://scoresaber.com
-    static const std::string BASE_URL = "http://192.168.1.8:9999";
+    static const std::string BASE_URL = "https://scoresaber.com";
 
-    static const std::string DATA_DIR = "/sdcard/ModData/Mods/ScoreSaber";
+    static const std::string DATA_DIR = "/sdcard/ModData/com.beatgames.beatsaber/Mods/ScoreSaber";
     static const std::string STEAM_KEY_PATH = DATA_DIR + "/scoresaber_DO_NOT_SHARE.scary";
 
     static const std::string REPLAY_DIR = DATA_DIR + "/replays";
