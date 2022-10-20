0eb6566c13d8b8b5f8892d2f604a2a56f82fddde
Umbranox <itsumbra@protonmail.com>
2022-10-21 06:04:05 +1100

    MultiplierPlayer done

--- include/ReplaySystem/Playback/MultiplierPlayer.hpp
+++ include/ReplaySystem/Playback/MultiplierPlayer.hpp
@@ -8,10 +8,10 @@
 #include "lapiz/shared/macros.hpp"
 
 DECLARE_CLASS_CODEGEN(
-    ScoreSaber::ReplaySystem::Playback, EnergyPlayer, Il2CppObject,
+    ScoreSaber::ReplaySystem::Playback, MultiplierPlayer, Il2CppObject,
     DECLARE_PRIVATE_FIELD(GlobalNamespace::AudioTimeSyncController*, _audioTimeSyncController);
     DECLARE_PRIVATE_FIELD(GlobalNamespace::ScoreController*, _scoreController);
     DECLARE_CTOR(ctor, GlobalNamespace::AudioTimeSyncController* audioTimeSyncController, GlobalNamespace::ScoreController* _scoreController);
     DECLARE_INSTANCE_METHOD(void, TimeUpdate, float songTime);
-    vector<Data::Private::EnergyEvent> _sortedEnergyEvents;
+    vector<Data::Private::MultiplierEvent> _sortedMultiplierEvents;
     void UpdateMultiplier(int multiplier, float progress);)
---
+++ src/ReplaySystem/Playback/MultiplierPlayer.cpp
@@ -0,0 +1,46 @@
+#include "ReplaySystem/Playback/MultiplierPlayer.hpp"
+#include "ReplaySystem/ReplayLoader.hpp"
+#include "System/Action_2.hpp"
+#include "logging.hpp"
+#include <algorithm>
+
+using namespace UnityEngine;
+using namespace ScoreSaber::Data::Private;
+
+DEFINE_TYPE(ScoreSaber::ReplaySystem::Playback, MultiplierPlayer);
+
+namespace ScoreSaber::ReplaySystem::Playback
+{
+    void MultiplierPlayer::ctor(GlobalNamespace::AudioTimeSyncController* audioTimeSyncController, GlobalNamespace::ScoreController* _scoreController)
+    {
+        _audioTimeSyncController = audioTimeSyncController;
+        _scoreController = _scoreController;
+        _sortedMultiplierEvents = ReplayLoader::LoadedReplay->multiplierKeyframes;
+    }
+    void MultiplierPlayer::TimeUpdate(float newTime)
+    {
+        for (int c = 0; c < _sortedMultiplierEvents.size(); c++)
+        {
+            if (_sortedMultiplierEvents[c].Time >= newTime)
+            {
+                int multiplier = c != 0 ? _sortedMultiplierEvents[c - 1].Multiplier : 1;
+                float progress = c != 0 ? _sortedMultiplierEvents[c - 1].NextMultiplierProgress : 0.0f;
+                UpdateMultiplier(multiplier, progress);
+                return;
+            }
+        }
+        auto lastEvent = _sortedMultiplierEvents[_sortedMultiplierEvents.size() - 1];
+        UpdateMultiplier(lastEvent.Multiplier, lastEvent.NextMultiplierProgress);
+    }
+    void MultiplierPlayer::UpdateMultiplier(int multiplier, float progress)
+    {
+        auto counter = _scoreController->scoreMultiplierCounter;
+        counter->multiplier = multiplier;
+        counter->multiplierIncreaseMaxProgress = multiplier * 2;
+        counter->multiplierIncreaseProgress = (int)(progress * (multiplier * 2));
+        if (_scoreController->multiplierDidChangeEvent != nullptr)
+        {
+            _scoreController->multiplierDidChangeEvent->Invoke(multiplier, progress);
+        }
+    }
+} // namespace ScoreSaber::ReplaySystem::Playback
