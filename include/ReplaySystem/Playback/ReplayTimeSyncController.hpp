436973bf6ce98fcb630076a0ede75745934d7b4f
Umbranox <itsumbra@protonmail.com>
2022-10-21 06:38:06 +1100

    Finish up ReplayTimeSyncController

--- include/ReplaySystem/Playback/ReplayTimeSyncController.hpp
+++ include/ReplaySystem/Playback/ReplayTimeSyncController.hpp
@@ -7,10 +7,14 @@
 #include "GlobalNamespace/BasicBeatmapObjectManager.hpp"
 #include "GlobalNamespace/BeatmapCallbacksController.hpp"
 #include "GlobalNamespace/BeatmapCallbacksController_InitData.hpp"
-#include "GlobalNamespace/BeatmapObjectSpawnController.hpp"
 #include "GlobalNamespace/NoteCutSoundEffectManager.hpp"
+#include "ReplaySystem/Playback/ComboPlayer.hpp"
+#include "ReplaySystem/Playback/EnergyPlayer.hpp"
+#include "ReplaySystem/Playback/HeightPlayer.hpp"
+#include "ReplaySystem/Playback/MultiplierPlayer.hpp"
 #include "ReplaySystem/Playback/NotePlayer.hpp"
 #include "ReplaySystem/Playback/PosePlayer.hpp"
+#include "ReplaySystem/Playback/ScorePlayer.hpp"
 #include "System/IDisposable.hpp"
 #include "Zenject/DiContainer.hpp"
 #include "Zenject/IInitializable.hpp"
@@ -30,11 +34,16 @@ ___DECLARE_TYPE_WRAPPER_INHERITANCE(ScoreSaber::ReplaySystem::Playback, ReplayTi
                                     DECLARE_PRIVATE_FIELD(GlobalNamespace::BasicBeatmapObjectManager*, _basicBeatmapObjectManager);
                                     DECLARE_PRIVATE_FIELD(GlobalNamespace::NoteCutSoundEffectManager*, _noteCutSoundEffectManager);
                                     DECLARE_PRIVATE_FIELD(GlobalNamespace::BeatmapCallbacksController::InitData*, _callbackInitData);
-                                    DECLARE_PRIVATE_FIELD(GlobalNamespace::BeatmapObjectSpawnController*, _beatmapObjectCallbackController);
-                                    DECLARE_PRIVATE_FIELD(ScoreSaber::ReplaySystem::Playback::PosePlayer*, _posePlayer);
+                                    DECLARE_PRIVATE_FIELD(GlobalNamespace::BeatmapCallbacksController*, _beatmapObjectCallbackController);
+                                    DECLARE_PRIVATE_FIELD(ScoreSaber::ReplaySystem::Playback::ComboPlayer*, _comboPlayer);
+                                    DECLARE_PRIVATE_FIELD(ScoreSaber::ReplaySystem::Playback::EnergyPlayer*, _energyPlayer);
+                                    DECLARE_PRIVATE_FIELD(ScoreSaber::ReplaySystem::Playback::HeightPlayer*, _heightPlayer);
+                                    DECLARE_PRIVATE_FIELD(ScoreSaber::ReplaySystem::Playback::MultiplierPlayer*, _multiplierPlayer);
                                     DECLARE_PRIVATE_FIELD(ScoreSaber::ReplaySystem::Playback::NotePlayer*, _notePlayer);
+                                    DECLARE_PRIVATE_FIELD(ScoreSaber::ReplaySystem::Playback::PosePlayer*, _posePlayer);
+                                    DECLARE_PRIVATE_FIELD(ScoreSaber::ReplaySystem::Playback::ScorePlayer*, _scorePlayer);
                                     DECLARE_PRIVATE_FIELD(bool, _paused);
-                                    DECLARE_CTOR(ctor, GlobalNamespace::AudioTimeSyncController* audioTimeSyncController, GlobalNamespace::AudioTimeSyncController::InitData* audioInitData, GlobalNamespace::BasicBeatmapObjectManager* basicBeatmapObjectManager, GlobalNamespace::NoteCutSoundEffectManager* _noteCutSoundEffectManager, GlobalNamespace::BeatmapCallbacksController::InitData* callbackInitData, GlobalNamespace::BeatmapObjectSpawnController* beatmapObjectCallbackController, ScoreSaber::ReplaySystem::Playback::PosePlayer* posePlayer, ScoreSaber::ReplaySystem::Playback::NotePlayer* notePlayer);
+                                    DECLARE_CTOR(ctor, GlobalNamespace::AudioTimeSyncController* audioTimeSyncController, GlobalNamespace::AudioTimeSyncController::InitData* audioInitData, GlobalNamespace::BasicBeatmapObjectManager* basicBeatmapObjectManager, GlobalNamespace::NoteCutSoundEffectManager* _noteCutSoundEffectManager, GlobalNamespace::BeatmapCallbacksController::InitData* callbackInitData, GlobalNamespace::BeatmapCallbacksController* beatmapObjectCallbackController, ScoreSaber::ReplaySystem::Playback::ComboPlayer* comboPlayer, ScoreSaber::ReplaySystem::Playback::EnergyPlayer* energyPlayer, ScoreSaber::ReplaySystem::Playback::HeightPlayer* heightPlayer, ScoreSaber::ReplaySystem::Playback::MultiplierPlayer* multiplierPlayer, ScoreSaber::ReplaySystem::Playback::NotePlayer* notePlayer, ScoreSaber::ReplaySystem::Playback::PosePlayer* posePlayer, ScoreSaber::ReplaySystem::Playback::ScorePlayer* scorePlayer);
                                     DECLARE_OVERRIDE_METHOD(void, Tick, il2cpp_utils::il2cpp_type_check::MetadataGetter<&::Zenject::ITickable::Tick>::get());
                                     DECLARE_INSTANCE_METHOD(void, UpdateTimes);
                                     DECLARE_INSTANCE_METHOD(void, OverrideTime, float time);
--- src/ReplaySystem/Playback/ReplayTimeSyncController.cpp
+++ src/ReplaySystem/Playback/ReplayTimeSyncController.cpp
@@ -1,6 +1,12 @@
 
 #include "ReplaySystem/Playback/ReplayTimeSyncController.hpp"
+#include "GlobalNamespace/CallbacksInTime.hpp"
+#include "System/Collections/Generic/Dictionary_2.hpp"
+#include "System/Collections/Generic/LinkedListNode_1.hpp"
+#include "UnityEngine/AudioSource.hpp"
+#include "UnityEngine/GameObject.hpp"
 #include "UnityEngine/Mathf.hpp"
+#include "UnityEngine/Time.hpp"
 
 #include "logging.hpp"
 
@@ -16,9 +22,14 @@ namespace ScoreSaber::ReplaySystem::Playback
                                         GlobalNamespace::BasicBeatmapObjectManager* basicBeatmapObjectManager,
                                         GlobalNamespace::NoteCutSoundEffectManager* _noteCutSoundEffectManager,
                                         GlobalNamespace::BeatmapCallbacksController::InitData* callbackInitData,
-                                        GlobalNamespace::BeatmapObjectSpawnController* beatmapObjectCallbackController,
+                                        GlobalNamespace::BeatmapCallbacksController* beatmapObjectCallbackController,
+                                        ScoreSaber::ReplaySystem::Playback::ComboPlayer* comboPlayer,
+                                        ScoreSaber::ReplaySystem::Playback::EnergyPlayer* energyPlayer,
+                                        ScoreSaber::ReplaySystem::Playback::HeightPlayer* heightPlayer,
+                                        ScoreSaber::ReplaySystem::Playback::MultiplierPlayer* multiplierPlayer,
+                                        ScoreSaber::ReplaySystem::Playback::NotePlayer* notePlayer,
                                         ScoreSaber::ReplaySystem::Playback::PosePlayer* posePlayer,
-                                        ScoreSaber::ReplaySystem::Playback::NotePlayer* notePlayer)
+                                        ScoreSaber::ReplaySystem::Playback::ScorePlayer* scorePlayer)
     {
         _audioTimeSyncController = audioTimeSyncController;
         _audioInitData = audioInitData;
@@ -29,8 +40,13 @@ namespace ScoreSaber::ReplaySystem::Playback
 
         _audioManagerSO = _noteCutSoundEffectManager->audioManager;
 
-        _posePlayer = posePlayer;
+        _comboPlayer = comboPlayer;
+        _energyPlayer = energyPlayer;
+        _heightPlayer = heightPlayer;
+        _multiplierPlayer = multiplierPlayer;
         _notePlayer = notePlayer;
+        _posePlayer = posePlayer;
+        _scorePlayer = scorePlayer;
     }
 
     void ReplayTimeSyncController::Tick()
@@ -40,8 +56,13 @@ namespace ScoreSaber::ReplaySystem::Playback
 
     void ReplayTimeSyncController::UpdateTimes()
     {
-        _posePlayer->TimeUpdate(_audioTimeSyncController->songTime);
+        _comboPlayer->TimeUpdate(_audioTimeSyncController->songTime);
+        _energyPlayer->TimeUpdate(_audioTimeSyncController->songTime);
+        _heightPlayer->TimeUpdate(_audioTimeSyncController->songTime);
+        _multiplierPlayer->TimeUpdate(_audioTimeSyncController->songTime);
         _notePlayer->TimeUpdate(_audioTimeSyncController->songTime);
+        _posePlayer->TimeUpdate(_audioTimeSyncController->songTime);
+        _scorePlayer->TimeUpdate(_audioTimeSyncController->songTime);
     }
 
     void ReplayTimeSyncController::OverrideTime(float time)
@@ -50,14 +71,95 @@ namespace ScoreSaber::ReplaySystem::Playback
         {
             return;
         }
+
+        // TODO: Cancel hitsounds
+
+        CancelAllHitSounds();
+
+        auto gameNotePool = _basicBeatmapObjectManager->basicGameNotePoolContainer;
+        auto gameNotePoolItems = gameNotePool->get_activeItems();
+        for (int i = 0; i < gameNotePoolItems->get_Count(); i++)
+        {
+            auto item = gameNotePoolItems->get_Item(i);
+            item->Hide(false);
+            item->Pause(false);
+            item->set_enabled(true);
+            item->get_gameObject()->SetActive(true);
+            item->Dissolve(0.0f);
+        }
+
+        auto bombNotePool = _basicBeatmapObjectManager->bombNotePoolContainer;
+        auto bombNotePoolItems = bombNotePool->get_activeItems();
+        for (int i = 0; i < bombNotePoolItems->get_Count(); i++)
+        {
+            auto item = bombNotePoolItems->get_Item(i);
+            item->Hide(false);
+            item->Pause(false);
+            item->set_enabled(true);
+            item->get_gameObject()->SetActive(true);
+            item->Dissolve(0.0f);
+        }
+
+        for (int i = 0; i < _basicBeatmapObjectManager->get_activeObstacleControllers()->get_Count(); i++)
+        {
+            auto item = _basicBeatmapObjectManager->get_activeObstacleControllers()->get_Item(i);
+            item->Hide(false);
+            item->Pause(false);
+            item->set_enabled(true);
+            item->get_gameObject()->SetActive(true);
+            item->Dissolve(0.0f);
+        }
+
+        auto previousState = _audioTimeSyncController->state;
+        _audioTimeSyncController->Pause();
+        _audioTimeSyncController->SeekTo(time / _audioTimeSyncController->timeScale);
+
+        if (previousState == GlobalNamespace::AudioTimeSyncController::State::Playing)
+        {
+            _audioTimeSyncController->Resume();
+        }
+
+        _callbackInitData->startFilterTime = time;
+        _beatmapObjectCallbackController->startFilterTime = time;
+        auto callbacks = _beatmapObjectCallbackController->callbacksInTimes;
+        for (int i = 0; i < callbacks->get_Count(); i++)
+        {
+            auto callback = callbacks->get_Item(i);
+            if (callback->lastProcessedNode != nullptr && callback->lastProcessedNode->item->time > time)
+            {
+                callback->lastProcessedNode = nullptr;
+            }
+        }
+
+        _audioTimeSyncController->songTime = time;
+        _audioTimeSyncController->Update();
+        UpdateTimes();
     }
 
-    void ReplayTimeSyncController::OverrideTimeScale(float timeScale)
+    void ReplayTimeSyncController::OverrideTimeScale(float newScale)
     {
+        CancelAllHitSounds();
+        _audioTimeSyncController->audioSource->set_pitch(newScale);
+        _audioTimeSyncController->timeScale = newScale;
+        _audioTimeSyncController->audioStartTimeOffsetSinceStart = (Time::get_timeSinceLevelLoad() * _audioTimeSyncController->timeScale) - (_audioTimeSyncController->songTime + _audioInitData->songTimeOffset);
+
+        _audioManagerSO->set_musicPitch(1.0f / newScale);
+        _audioTimeSyncController->Update();
     }
 
     void ReplayTimeSyncController::CancelAllHitSounds()
     {
+        auto noteCutPool = _noteCutSoundEffectManager->noteCutSoundEffectPoolContainer;
+        auto noteCutPoolItems = noteCutPool->get_activeItems();
+        for (int i = 0; i < noteCutPoolItems->get_Count(); i++)
+        {
+            auto effect = noteCutPoolItems->get_Item(i);
+            if (effect->get_isActiveAndEnabled())
+            {
+                effect->StopPlayingAndFinish();
+            }
+        }
+        _noteCutSoundEffectManager->prevNoteATime = -1.0f;
+        _noteCutSoundEffectManager->prevNoteBTime = -1.0f;
     }
-
 } // namespace ScoreSaber::ReplaySystem::Playback
