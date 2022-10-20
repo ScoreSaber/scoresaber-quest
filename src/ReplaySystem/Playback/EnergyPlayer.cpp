5e148967ccfcbf8926453131a0332d3a4c05e045
Umbranox <itsumbra@protonmail.com>
2022-10-21 05:58:20 +1100

    EnergyPlayer done

---
+++ src/ReplaySystem/Playback/EnergyPlayer.cpp
@@ -0,0 +1,76 @@
+#include "ReplaySystem/Playback/EnergyPlayer.hpp"
+#include "ReplaySystem/ReplayLoader.hpp"
+#include "System/Action_1.hpp"
+#include "UnityEngine/Mathf.hpp"
+#include "UnityEngine/Playables/PlayableDirector.hpp"
+#include "UnityEngine/UI/Image.hpp"
+#include "logging.hpp"
+#include <algorithm>
+
+using namespace UnityEngine;
+using namespace ScoreSaber::Data::Private;
+
+DEFINE_TYPE(ScoreSaber::ReplaySystem::Playback, EnergyPlayer);
+
+namespace ScoreSaber::ReplaySystem::Playback
+{
+    void EnergyPlayer::ctor(GlobalNamespace::AudioTimeSyncController* audioTimeSyncController, GlobalNamespace::GameEnergyCounter* _gameEnergyCounter, GlobalNamespace::GameEnergyUIPanel* _gameEnergyUIPanel)
+    {
+        _audioTimeSyncController = audioTimeSyncController;
+        _gameEnergyCounter = _gameEnergyCounter;
+        _gameEnergyUIPanel = _gameEnergyUIPanel;
+        _sortedEnergyEvents = ReplayLoader::LoadedReplay->energyKeyframes;
+    }
+
+    void EnergyPlayer::TimeUpdate(float newTime)
+    {
+        if (_gameEnergyUIPanel == nullptr)
+        {
+            return;
+        }
+        for (int c = 0; c < _sortedEnergyEvents.size(); c++)
+        {
+            if (_sortedEnergyEvents[c].Time >= newTime)
+            {
+                float energy = c != 0 ? _sortedEnergyEvents[c - 1].Energy : 0.5f;
+                UpdateEnergy(energy);
+                return;
+            }
+        }
+        UpdateEnergy(0.5f);
+        auto lastEvent = _sortedEnergyEvents[_sortedEnergyEvents.size() - 1];
+        if (newTime >= lastEvent.Time && lastEvent.Energy <= Mathf::_get_Epsilon())
+        {
+            UpdateEnergy(0.0f);
+        }
+    }
+
+    void EnergyPlayer::UpdateEnergy(float energy)
+    {
+        if (_gameEnergyUIPanel == nullptr)
+        {
+            return;
+        }
+
+        bool isFailingEnergy = energy <= Mathf::_get_Epsilon();
+        bool noFail = _gameEnergyCounter->noFail;
+        _gameEnergyCounter->noFail = false;
+        _gameEnergyCounter->didReach0Energy = isFailingEnergy;
+        _gameEnergyCounter->ProcessEnergyChange(energy);
+        _gameEnergyCounter->nextFrameEnergyChange = 0.0f;
+        _gameEnergyCounter->energy = energy;
+        _gameEnergyCounter->noFail = noFail;
+
+        _gameEnergyUIPanel->Init();
+        auto director = _gameEnergyUIPanel->playableDirector;
+        director->Stop();
+        director->Evaluate();
+        _gameEnergyUIPanel->energyBar->set_enabled(!isFailingEnergy);
+        if (_gameEnergyCounter->gameEnergyDidChangeEvent != nullptr)
+        {
+            _gameEnergyCounter->gameEnergyDidChangeEvent->Invoke(energy);
+        }
+        return;
+    }
+
+} // namespace ScoreSaber::ReplaySystem::Playback
