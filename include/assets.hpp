85005df927118b6b95e4e32c4acc5040f43bdaed
Umbranox <itsumbra@protonmail.com>
2022-10-21 06:39:26 +1100

    Fix assets include

--- include/assets.hpp
+++ include/assets.hpp
@@ -1,55 +1,48 @@
 #pragma once
 
-#include "beatsaber-hook/shared/utils/typedefs.h"
 #include <string_view>
+#include "beatsaber-hook/shared/utils/typedefs.h"
 
-struct IncludedAsset
-{
+struct IncludedAsset {
 
-    IncludedAsset(uint8_t* start, uint8_t* end)
-        : array(reinterpret_cast<Array<uint8_t>*>(start))
-    {
+    IncludedAsset(uint8_t* start, uint8_t* end) : array(reinterpret_cast<Array<uint8_t>*>(start)) {
         array->klass = nullptr;
         array->monitor = nullptr;
         array->bounds = nullptr;
         array->max_length = end - start - 33;
-        *(end - 1) = '\0';
+        *(end - 1)= '\0';
     }
-
-    operator ArrayW<uint8_t>() const
-    {
+    
+    operator ArrayW<uint8_t>() const {
         init();
         return array;
     }
 
-    operator std::string_view() const
-    {
-        return {reinterpret_cast<char*>(array->values), array->Length()};
+    operator std::string_view() const {
+        return { reinterpret_cast<char*>(array->values), array->Length() };
+    }
+    
+    operator std::span<uint8_t>() const {
+        return { array->values, array->Length() };
     }
 
-    operator std::span<uint8_t>() const
-    {
-        return {array->values, array->Length()};
-    }
-
-    void init() const
-    {
-        if (!array->klass)
+    void init() const {
+        if(!array->klass)
             array->klass = classof(Array<uint8_t>*);
     }
 
-  private:
-    Array<uint8_t>* array;
+    private:
+        Array<uint8_t>* array;
+
 };
 
-#define DECLARE_FILE(name)                       \
-    extern "C" uint8_t _binary_##name##_start[]; \
-    extern "C" uint8_t _binary_##name##_end[];   \
-    const IncludedAsset name{_binary_##name##_start, _binary_##name##_end};
+#define DECLARE_FILE(name)                         \
+    extern "C" uint8_t _binary_##name##_start[];  \
+    extern "C" uint8_t _binary_##name##_end[];    \
+    const IncludedAsset name { _binary_##name##_start, _binary_##name##_end};
 
-namespace IncludedAssets
-{
+namespace IncludedAssets {
 
-    DECLARE_FILE(Bundle)
+	DECLARE_FILE(imber_panel_bsml)
 
 }
