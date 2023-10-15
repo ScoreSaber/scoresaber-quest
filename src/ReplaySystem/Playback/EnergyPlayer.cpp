404f6febcb8378d352b4097a4317787630c3452d
Qwasyx <qwasyx3000@gmail.com>
2023-10-15 19:11:39 +0200

    fix EnergyPlayer when there is no GameEnergyUIPanel

--- src/ReplaySystem/Playback/EnergyPlayer.cpp
+++ src/ReplaySystem/Playback/EnergyPlayer.cpp
@@ -15,21 +15,16 @@ DEFINE_TYPE(ScoreSaber::ReplaySystem::Playback, EnergyPlayer);
 
 namespace ScoreSaber::ReplaySystem::Playback
 {
-    void EnergyPlayer::ctor(GlobalNamespace::AudioTimeSyncController* audioTimeSyncController, GlobalNamespace::GameEnergyCounter* gameEnergyCounter)
+    void EnergyPlayer::ctor(GlobalNamespace::AudioTimeSyncController* audioTimeSyncController, GlobalNamespace::GameEnergyCounter* gameEnergyCounter, Zenject::DiContainer* container)
     {
         _audioTimeSyncController = audioTimeSyncController;
         _gameEnergyCounter = gameEnergyCounter;
-        _gameEnergyUIPanel = UnityEngine::Resources::FindObjectsOfTypeAll<GlobalNamespace::GameEnergyUIPanel*>()->values[0];
+        _gameEnergyUIPanel = container->TryResolve<GlobalNamespace::GameEnergyUIPanel*>();
         _sortedEnergyEvents = ReplayLoader::LoadedReplay->energyKeyframes;
     }
 
     void EnergyPlayer::TimeUpdate(float newTime)
     {
-        // TODO: How would this ever be null? We get the object via Zenject and that would fail if it was null     
-        if (_gameEnergyUIPanel == nullptr)
-        {
-            return;
-        }
         for (int c = 0; c < _sortedEnergyEvents.size(); c++)
         {
             // TODO: this has potential to have problems if _sortedEnergyEvents[c].Time is within an epsilon of newTime, potentially applying energy changes twice or not at all
@@ -50,11 +45,6 @@ namespace ScoreSaber::ReplaySystem::Playback
 
     void EnergyPlayer::UpdateEnergy(float energy)
     {
-        if (_gameEnergyUIPanel == nullptr)
-        {
-            return;
-        }
-
         bool isFailingEnergy = energy <= Mathf::_get_Epsilon();
         bool noFail = _gameEnergyCounter->noFail;
         _gameEnergyCounter->noFail = false;
@@ -64,11 +54,13 @@ namespace ScoreSaber::ReplaySystem::Playback
         _gameEnergyCounter->energy = energy;
         _gameEnergyCounter->noFail = noFail;
 
-        _gameEnergyUIPanel->Init();
-        auto director = _gameEnergyUIPanel->playableDirector;
-        director->Stop();
-        director->Evaluate();
-        _gameEnergyUIPanel->energyBar->set_enabled(!isFailingEnergy);
+        if (_gameEnergyUIPanel != nullptr) {
+            _gameEnergyUIPanel->Init();
+            auto director = _gameEnergyUIPanel->playableDirector;
+            director->Stop();
+            director->Evaluate();
+            _gameEnergyUIPanel->energyBar->set_enabled(!isFailingEnergy);
+        }
         if (_gameEnergyCounter->gameEnergyDidChangeEvent != nullptr)
         {
             _gameEnergyCounter->gameEnergyDidChangeEvent->Invoke(energy);
