fd7ffbbc02b97607eaa9795c22f9cadaf95d9f9c
Riley <105916526@student.swin.edu.au>
2025-12-17 12:23:54 +1100

    fix combo calculation once more

--- include/ReplaySystem/Playback/ComboPlayer.hpp
+++ include/ReplaySystem/Playback/ComboPlayer.hpp
@@ -16,7 +16,4 @@ DECLARE_CLASS_CODEGEN(ScoreSaber::ReplaySystem::Playback, ComboPlayer, Il2CppObj
     vector<Data::Private::NoteEvent> _sortedNoteEvents;
     vector<Data::Private::ComboEvent> _sortedComboEvents;
     void UpdateCombo(float time, int combo);
-
-    private:
-    int highestCombo = 0;
 };
